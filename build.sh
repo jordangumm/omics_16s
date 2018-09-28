7e70abd05cc7eb8096fb9451556a6ae7c63c8120
Jordan Gumm <jngumm@gmail.com>
2018-09-28 10:35:05 -0400

    updated build script

--- build.sh
+++ build.sh
@@ -23,8 +23,9 @@ source ./dependencies/miniconda/bin/activate
 # add bioconda and r channel for easy dependency installations
 conda config --add channels r
 conda config --add channels bioconda
-conda install -c bioconda mothur bioconductor-ggtree fasttree blast
-conda install -c conda-forge readline
+pip install screed
+conda install -c bioconda mothur bioconductor-ggtree fasttree blast -y
+conda install -c conda-forge readline -y
 
 # install pyflow for automated task management
 wget https://github.com/Illumina/pyflow/releases/download/v1.1.17/pyflow-1.1.17.tar.gz
@@ -45,15 +46,30 @@ wget https://raw.githubusercontent.com/nisheth/MeFiT/master/mefit
 chmod 772 mefit
 mv mefit dependencies/miniconda/bin/
 
-#git clone https://github.com/McMahonLab/TaxAss.git
-#unzip TaxAss/FreshTrain-files/FreshTrain18Aug2016.zip
-#mv TaxAss dependencies/
-#mv FreshTrain18Aug2016 dependencies/TaxAss/
+# install Silva databases
+mkdir -p dependencies/silva
+wget https://mothur.org/w/images/7/71/Silva.seed_v132.tgz
+tar -zxvf Silva.seed_v132.tgz -C dependencies/silva/
 
-#mkdir -p dependencies/silva
-#wget https://mothur.org/w/images/b/b4/Silva.nr_v128.tgz
-#tar -zxvf Silva.nr_v128.tgz -C dependencies/silva/
+wget https://mothur.org/w/images/3/32/Silva.nr_v132.tgz
+tar -zxvf Silva.nr_v132.tgz -C dependencies/silva/
+rm -r Silva*
 
-#wget https://mothur.org/w/images/a/a4/Silva.seed_v128.tgz
-#tar -zxvf Silva.seed_v128.tgz -C dependencies/silva/
-#rm -r Silva*
+# install TaxAss
+git clone https://github.com/McMahonLab/TaxAss.git
+unzip TaxAss/FreshTrain-files/FreshTrain18Aug2016.zip
+mv TaxAss dependencies/
+mv FreshTrain18Aug2016 dependencies/TaxAss/
+
+# install vsearch
+wget https://github.com/torognes/vsearch/archive/v2.8.5.tar.gz
+tar zxvf vsearch-2.8.5.tar.gz
+cd vsearch-2.8.5
+./autogen.sh
+./configure --prefix=$HOME
+make
+make install
+cp bin/vsearch ../dependencies/miniconda/bin/
+cd ..
+rm -r vsearch*
+rm v2.8.5.tar.gz
